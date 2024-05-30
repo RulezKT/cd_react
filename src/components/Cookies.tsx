62601b63f89afd45f692e0e308e5ad4dccb79599
RR <9381804@gmail.com>
2024-05-31 00:33:19 +0200

    adding ant design

--- src/components/Cookies.tsx
+++ src/components/Cookies.tsx
@@ -1,3 +1,30 @@
-const [last10, setLast10] = useState([]);
-
 import Cookies from "js-cookie";
+
+async function getCookies() {
+  const cookies = Cookies.get("last10");
+  // console.log(cookies);
+  const json = JSON.parse(cookies);
+  // console.log(json);
+  // setLast10(json);
+
+  // console.log(`getCookies ${json.length}`);
+  const { data } = await axios.post(
+    "http://127.0.0.1:3000/api/cookies",
+    {
+      cookies: json,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+
+        Accept: "*/*",
+      },
+    }
+  );
+
+  setLast10(data);
+}
+
+// =======================================
+
+Cookies.set("last10", JSON.stringify(json));
